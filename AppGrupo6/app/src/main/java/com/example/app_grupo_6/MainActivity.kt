34e4439ca2c290f0f052b2dbfba70b92aca73ba5
Yowin <62089624+blancohr19@users.noreply.github.com>
2021-11-10 11:45:47 -0500

    Creación Actividad main y Actividad Login

--- AppGrupo6/app/src/main/java/com/example/app_grupo_6/MainActivity.kt
+++ AppGrupo6/app/src/main/java/com/example/app_grupo_6/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.app_grupo_6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun OnLogin(botonLogin: View){
+        val intento = Intent(this,LoginActivity::class.java)
+        startActivity(intento)
+    }
+
 }
